c982baf763d6cc51d43f893ffa1fbdb55e141d34
Nadman Faisal <nadman@chalmers.se>
2025-02-04 09:32:34 +0100

    Additional comments to learning pointers

--- arrays_and_methods.c
+++ arrays_and_methods.c
@@ -2,19 +2,22 @@
 
 /*
 The following function takes a pointer to an array and prints the 
-memory addresses of its elements. Since arrays decay into pointers
-when passed to functions, the parameter `int *array` is equivalent
-to `int array[]` in function declarations.
+memory addresses of its elements. Since arrays decay into pointer 
+to the first element of the array when passed to functions, the 
+parameter `int *array` is equivalent to `int array[]` in function 
+declarations. So, array[] points to first element of the array.
 */
-void printer(int *array) {
-    printf("Memory in the method: \n");
+void printer(int array[]) {
+    printf("Memory of the array's first element in method: %p\n", array);
     for(int i = 0; i < 5; i++) {
-        printf("%p\n", &array[i]);      // Printing the address of each element
+        printf("%d's address in the loop: %p\n", array[i], &array[i]);
     }
 }
 
 int main(int argc, char *argv[]) {
     int numbers[] = {1, 2, 3, 4, 5};
+
+    printf("Memory of the array's first element in main: %p\n", &numbers);
 
     // // Printing the address of each element in main
     printf("Address 1: %p\n", &numbers[0]);
@@ -25,7 +28,7 @@ int main(int argc, char *argv[]) {
     printf("\n");
 
     /*
-    We pass `numbers` instead of `&numbers` because in C, when an array is passed
+    We can pass `numbers` instead of `&numbers` because in C, when an array is passed
     to a function, it automatically decays into a pointer to its first element (`&numbers[0]`).
     This means `printer(numbers)` is effectively passing `int *` (pointer to the first element).
     */
